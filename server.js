81515badc69ece95c6494db475aa05571ce220f6
sammy2511 <42390656+sammy2511@users.noreply.github.com>
2018-09-09 01:47:40 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear();
+})
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now} : ${req.method} : ${req.url}`;
+
+  fs.appendFile('server.log',log + '\n',(error) => {
+    if (error) {
+      console.log('Unable to append in server.log');
+    }
+  });
+next();
+});
+
+
+app.use((req,res) => {
+  res.render('maintainence.hbs')
+})
+
+app.get('/',(req,res) => {
+  res.render('home.hbs',{
+    pageTitle:'Home Page',
+    welcomeMessage:'Welcome to Express App'
+  });
+});
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs',{
+    pageTitle:'About Page'
+  });
+});
+
+app.listen(3000);
